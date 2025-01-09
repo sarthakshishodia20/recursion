80134e43234c04a35ecbab06e755da5b6de0391f
SARTHAK SHISHODIA <122453870+sarthakshishodia20@users.noreply.github.com>
2025-01-09 17:51:41 +0530

    Create LongestCommonSubsequence.java

---
+++ Recursion.java/LongestCommonSubsequence.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        return usingTabulation(text1,text1.length(),text2,text2.length());
+    }
+    public static int usingTabulation(String str1,int n,String str2,int m){
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0||j==0)
+                {
+                     dp[i][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    int ans1=dp[i-1][j];
+                    int ans2=dp[i][j-1];
+                    dp[i][j]=Math.max(ans1,ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
